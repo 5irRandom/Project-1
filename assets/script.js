ae19427847450754eeb8670a4266a268ed915ceb
Jaxon D <duncan5irjax@gmail.com>
2021-02-03 18:58:45 -0600

    Fixed the text field call

--- assets/script.js
+++ assets/script.js
@@ -7,11 +7,12 @@
 // optional: display previous searches
 var lyricsAPI = "https://lyricsovh.docs.apiary.io/#reference";
 var searchURL = "https://api.lyrics.ovh/v1/";
+var youtubeURL = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyABnDA061k_mkfDbZlLVxHUepez69vgNCE&type=video&q=";
 var artist = "";
 var title = "";
 var searchButton = document.getElementById("search");
 
-function getLyrics() {
+function getLyrics(artist, title) {
     fetch(searchURL + artist + "/" + title)
         .then(function (response) {
             console.log(response);
@@ -25,7 +26,7 @@ function getLyrics() {
 
 // Minor WIP but it does work by just inputting a search term, it will then output 5 results to the console and the first one's video link
 function getVideo(searchTerm) {
-    fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyABnDA061k_mkfDbZlLVxHUepez69vgNCE&q=' + searchTerm + '&type=video')
+    fetch(youtubeURL + searchTerm)
     .then(response => {
         return response.json()
     })
@@ -40,8 +41,9 @@ function getVideo(searchTerm) {
 }
 
 searchButton.addEventListener("click", function (event) {
-    artistInput.textContent = artist;
-    songInput.textContent = title;
-    getLyrics();
-    getVideo(songInput.textContent); //Just an example of how we could implement it, it just outputs it to the console for now
+    artist = document.getElementById("artistInput").value;
+    title = document.getElementById("songInput").value;
+    console.log(title);
+    getLyrics(artist, title);
+    getVideo(title); //Just an example of how we could implement it, it just outputs it to the console for now
 })
