4242e04ec8ab5f6d5f897f81fabc23a7fd45a201
Jaxon D <duncan5irjax@gmail.com>
2021-02-06 11:24:00 -0600

    Updated the js to use textContent as well as adjust the frame's source so the rest can be controlled via html

--- assets/result.js
+++ assets/result.js
@@ -3,3 +3,9 @@ var lyrics = sessionStorage.getItem("lyrics");
 
 console.log(videoLink);
 console.log(lyrics);
+
+var videoSpot = document.getElementById('video');
+var lyricSpot = document.getElementById('lyrics')
+
+lyricSpot.textContent = lyrics;
+videoSpot.setAttribute('src', videoLink);
