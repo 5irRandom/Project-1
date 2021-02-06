0130397edf91068d9b5e44daf3975d0f98ca5a6e
caroline-e-miller <caroline.miller@ymail.com>
2021-02-06 10:33:01 -0600

    append attempt

--- assets/result.js
+++ assets/result.js
@@ -3,3 +3,9 @@ var lyrics = sessionStorage.getItem("lyrics");
 
 console.log(videoLink);
 console.log(lyrics);
+
+var videoDiv = document.getElementById("video");
+var lyricsDiv = document.getElementById("lyrics");
+
+videoDiv.append(videoLink);
+lyricsDiv.append(lyrics);
