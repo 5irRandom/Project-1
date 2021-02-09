fad46a0d29f1711a72bf680d3c4b52fc25694253
Jaxon D <duncan5irjax@gmail.com>
2021-02-08 20:00:37 -0600

    Updated the previous search buttons to allow more more as in, it allows for spaces, and can be edited more directly

--- assets/result.js
+++ assets/result.js
@@ -1,13 +1,16 @@
+// Pulls from sessionStorage
 var videoLink = sessionStorage.getItem("videoLink");
 var lyrics = sessionStorage.getItem("lyrics");
 
-console.log(videoLink);
-console.log(lyrics);
-
-
+// Sets variables for the placements
 var videoSpot = document.getElementById('video');
 var lyricSpot = document.getElementById('lyrics')
 
+// Just in case
+console.log(videoLink);
+console.log(lyrics);
+
+// Actually attaches the content gotten from sessionStorage
 lyricSpot.textContent = lyrics;
 videoSpot.setAttribute('src', videoLink);
 
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Setting variables and clearing session storage
 sessionStorage.clear();
 var lyricsAPI = "https://lyricsovh.docs.apiary.io/#reference";
 var searchURL = "https://api.lyrics.ovh/v1/";
@@ -5,14 +6,18 @@ var youtubeURL = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyABnDA06
 var artist = "";
 var title = "";
 var searchButton = document.getElementById("search");
+var clearButton = document.getElementById("clear");
 var lyricsDone = false;
 var videoDone = false;
-var storageString = "";
-storageString = localStorage.getItem('storage');
-if (storageString !== null) {
-    var buttonsStrings = storageString.split(", ");
+var titleString = localStorage.getItem("titleStorage");
+var artistString = localStorage.getItem("artistStorage")
+if (titleString !== null) {
+    var titleStorage = titleString.split(", ");
+    var artistStorage = artistString.split(", ");
 }
-var clearButton = document.getElementById("clear");
+else {
+    clearButton.style.display = "none"
+}
 
 // Calls the lyrics api and then stores it to sessionStorage
 function getLyrics(artist, title) {
@@ -33,22 +38,26 @@ function getLyrics(artist, title) {
 
 // Minor WIP but it does work by just inputting a search term, it will then first result's video link
 function getVideo(searchTerm) {
-    fetch(youtubeURL + searchTerm)
-    .then(response => {
-        return response.json()
-    })
-    .then (data => {
-        var videoId = data.items[0].id.videoId
-        var videoLink = "https://youtube.com/embed/" + videoId
-        sessionStorage.setItem("videoLink", videoLink)
-        videoDone = true;
-    })
-    .catch(err => {
-        console.log("Error: " + err)
-        document.getElementById("modal").style.display = "block"
-    });
+    var videoLink = "https://youtube.com/embed/" + "UqLRqzTp6Rk"
+    sessionStorage.setItem("videoLink", videoLink)
+    videoDone = true;
+    // fetch(youtubeURL + searchTerm)
+    // .then(response => {
+    //     return response.json()
+    // })
+    // .then (data => {
+    //     var videoId = data.items[0].id.videoId
+    //     var videoLink = "https://youtube.com/embed/" + videoId
+    //     sessionStorage.setItem("videoLink", videoLink)
+    //     videoDone = true;
+    // })
+    // .catch(err => {
+    //     console.log("Error: " + err)
+    //     document.getElementById("modal").style.display = "block"
+    // });
 }
 
+// Constant check to see if there is text in the fields, and if there isn't in one then it disables the search button
 setInterval(function(){
     if(!$('#artistInput').val() || !$('#songInput').val()) {
         searchButton.classList.add("disabled");
@@ -57,17 +66,19 @@ setInterval(function(){
     }
 }, 500);
 
+// This controls the local storage buttons, aka: the search history buttons
 function buttonCreate() {
     buttonPlace = document.getElementById("buttonPlace");
-    for (i = 1; i < buttonsStrings.length; i++) {
+    for (i = 1; i < titleStorage.length; i++) {
+        console.log(titleStorage[i]);
+        console.log(artistStorage[i]);
         var button = document.createElement("button");
-        button.textContent = buttonsStrings[i];
+        button.textContent = (titleStorage[i] + " " + artistStorage[i]);
         button.classList.add("button");
+        button.setAttribute("value", i);
         buttonPlace.appendChild(button);
         button.addEventListener("click", function (event) {
-            console.log(this.textContent)
-            var split = this.textContent.split(" ");
-            getLyrics(split[1], split[0]);
+            getLyrics(artistStorage[this.getAttribute('value')], titleStorage[this.getAttribute('value')]);
             getVideo(this.textContent);
             setInterval(function(){
                 if (videoDone === true && lyricsDone === true) {
@@ -78,26 +89,33 @@ function buttonCreate() {
     }
 }
 
+// Controls the clear button
 clearButton.addEventListener("click", function (event) {
     localStorage.clear();
     location.reload();
 })
 
-if (storageString !== null) {
-buttonCreate();
+// A check to see if storage is empty and if it isn't then run the storage button creation code
+if (titleString !== null) {
+    buttonCreate();
 }
 
+
+
+// Controls the search button as well as actually running the searches
 searchButton.addEventListener("click", function (event) {
+    // Quick check to see if the button is disabled
     if (searchButton.classList.contains("disabled")) {
 
     } else {
         artist = document.getElementById("artistInput").value;
         title = document.getElementById("songInput").value;
-        search = (title + " " + artist);
-        storageString += ", " + search;
-        localStorage.setItem('storage', storageString);
+        titleString += ", " + title;
+        artistString += ", " + artist;
+        localStorage.setItem('artistStorage', artistString);
+        localStorage.setItem('titleStorage', titleString);
         getLyrics(artist, title);
-        getVideo(search);
+        getVideo(title + " " + artist);
         setInterval(function(){
             if (videoDone === true && lyricsDone === true) {
                 document.location.href = 'results-page.html'
